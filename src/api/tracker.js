9002215b6d7fe076ed57a3348f092e34e4fd210d
Adam Morgan <adamcodes716@gmail.com>
2020-09-28 09:03:37 -0400

    Making an API push

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://9589b1ee9692.ngrok.io'
+ // baseURL: 'https://cors-anywhere.herokuapp.com/https://9589b1ee9692.ngrok.io',
+ baseURL: 'https://9589b1ee9692.ngrok.io'
 });
+
