b0a9b8a0263a6aba439fb79c2443f65fa15428fa
Adam Morgan <adamcodes716@gmail.com>
2020-09-28 09:13:37 -0400

    Handling errors

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
- // baseURL: 'https://cors-anywhere.herokuapp.com/https://9589b1ee9692.ngrok.io',
- baseURL: 'https://9589b1ee9692.ngrok.io'
+  baseURL: 'http://9589b1ee9692.ngrok.io',
 });
-
