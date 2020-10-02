f73eef5737bb335cfe64a85f7157e61a89953091
Adam Morgan <adamcodes716@gmail.com>
2020-10-02 11:30:51 -0400

    new auth resolver

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://77e3a885c7cb.ngrok.io',
+  baseURL: 'http://aae401fd5133.ngrok.io',
 });
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -9,12 +9,28 @@ const authReducer = (state, action) => {
       return { ...state, errorMessage: action.payload };
     case 'signin':
       return { errorMessage: '', token: action.payload };
+    case 'clear_error_message':
+      return { ...state, errorMessage: '' };
     default:
       return state;
   }
 };
 
-const signup = dispatch => async ({ email, password }) => {
+const tryLocalSignin = (dispatch) => async () => {
+  const token = await AsyncStorage.getItem('token');
+  if (token) {
+    dispatch({ type: 'signin', payload: token });
+    navigate('TrackList');
+  } else {
+    navigate('Signup');
+  }
+};
+
+const clearErrorMessage = (dispatch) => () => {
+  dispatch({ type: 'clear_error_message' });
+};
+
+const signup = (dispatch) => async ({ email, password }) => {
   try {
     const response = await trackerApi.post('/signup', { email, password });
     await AsyncStorage.setItem('token', response.data.token);
@@ -24,12 +40,12 @@ const signup = dispatch => async ({ email, password }) => {
   } catch (err) {
     dispatch({
       type: 'add_error',
-      payload: 'Something went wrong with sign up'
+      payload: 'Something went wrong with sign up',
     });
   }
 };
 
-const signin = dispatch => async ({ email, password }) => {
+const signin = (dispatch) => async ({ email, password }) => {
   try {
     const response = await trackerApi.post('/signin', { email, password });
     await AsyncStorage.setItem('token', response.data.token);
@@ -38,12 +54,12 @@ const signin = dispatch => async ({ email, password }) => {
   } catch (err) {
     dispatch({
       type: 'add_error',
-      payload: 'Something went wrong with sign in'
+      payload: 'Something went wrong with sign in',
     });
   }
 };
 
-const signout = dispatch => {
+const signout = (dispatch) => {
   return () => {
     // somehow sign out!!!
   };
@@ -51,6 +67,6 @@ const signout = dispatch => {
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { signin, signout, signup },
+  { signin, signout, signup, clearErrorMessage, tryLocalSignin },
   { token: null, errorMessage: '' }
 );
